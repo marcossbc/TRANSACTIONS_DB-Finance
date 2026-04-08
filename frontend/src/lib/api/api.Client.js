ab93ad8948d556725ad964f23ce3c8ad03d684d2
marcossbc <marcossbc880@gmail.com>
2026-04-08 13:05:17 +0300

    added finish deoply

--- frontend/src/lib/api/api.Client.js
+++ frontend/src/lib/api/api.Client.js
@@ -1,7 +1,7 @@
 import axios  from "axios";
 import useAuthStore from "../store/authStore";
 
-const API_URL =  "http://localhost:3000/api";
+const API_URL =  "https://transactions-db-finance.onrender.com/api";
 
  const api  = axios.create({
     baseURL : API_URL,
